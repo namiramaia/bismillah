17a7c1d37b5602499f2e51b0fbf92aaadcbad4e0
namiramaia <namiramaia29@gmail.com>
2026-05-20 09:45:07 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,22 +1,68 @@
-document.getElementById("loginForm").addEventListener("submit", function(e){
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function(e){
 
     e.preventDefault();
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("pwd").value;
+    let username =
+    document.getElementById("username").value;
 
-    // username & password contoh
-    if(username === "namira" && password === "123"){
+    let password =
+    document.getElementById("pwd").value;
 
-        localStorage.setItem("isLoggedIn", "true");
+    // cek apakah akun sudah pernah dibuat
+    let savedUser =
+    localStorage.getItem("username");
+
+    let savedPass =
+    localStorage.getItem("password");
+
+    // kalau belum ada akun → simpan akun baru
+    if(savedUser===null){
+
+        localStorage.setItem(
+        "username",
+        username
+        );
+
+        localStorage.setItem(
+        "password",
+        password
+        );
+
+        localStorage.setItem(
+        "isLoggedIn",
+        "true"
+        );
+
+        alert("Akun berhasil dibuat!");
+
+        window.location.href="../index.html";
+
+    }
+
+    // kalau akun sudah ada → cek login
+    else if(
+        username===savedUser &&
+        password===savedPass
+    ){
+
+        localStorage.setItem(
+        "isLoggedIn",
+        "true"
+        );
 
         alert("Login berhasil!");
 
         window.location.href="../index.html";
 
-    } else {
+    }
 
-        alert("Username atau password salah");
+    else{
+
+        alert(
+        "Username atau password salah"
+        );
 
     }
 
